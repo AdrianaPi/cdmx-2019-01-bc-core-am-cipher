f4ba76f69a0a6e81d187ced0584d3e1beb48f0c4
AdrianaPi <psic.adriana.a@gmail.com>
2019-01-15 12:34:07 -0600

    Construyendo codificador

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,19 @@
-window.cipher = {
-  // ... 
-};
+//window.cipher = {
+//const encode = () => {
+    //mensaje
+let mensajeInicial = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+
+    //recorrer mensaje inicial
+    for (i=0; i<mensajeInicial.length; i++){
+        console.log (mensajeInicial.charAt(i))
+    };
+    
+
+    //Conocer ASCII 
+let msjAsc = (mensajeInicial.charCodeAt(i));
+console.log (msjAsc);  
+ //}; // ...
+/*decode : () => {s
+
+}*/
+//};
