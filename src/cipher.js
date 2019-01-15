7bcc1ee270f0bebeeb6a16e0f9b3a0528b1dda76
AdrianaPi <psic.adriana.a@gmail.com>
2019-01-15 16:41:48 -0600

    Construyendo el codificador

--- src/cipher.js
+++ src/cipher.js
@@ -4,14 +4,21 @@
 let mensajeInicial = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
 
     //recorrer mensaje inicial
-    for (i=0; i<mensajeInicial.length; i++){
+    for (i=0; i<mensajeInicial.length; i++) {
         console.log (mensajeInicial.charAt(i))
-    };
-    
 
     //Conocer ASCII 
 let msjAsc = (mensajeInicial.charCodeAt(i));
-console.log (msjAsc);  
+console.log (msjAsc); 
+
+//Aplicar fórmula para codificar
+
+let cesarCod = ((msjAsc-65+33)%26+65);
+console.log (cesarCod)
+
+mensajeInicial.push (String.fromCharCode(cesarCod))
+
+    }
  //}; // ...
 /*decode : () => {s
 
